5a9f942c1f43c37b612f05cf78baf4c99fc30771
= <=>
2014-11-10 16:40:20 +0800

    这是什么问题呢？

---
+++ hello.py
@@ -0,0 +1,36 @@
+import socket
+
+s=raw_input("Input your age:")
+if s=="":
+   raise Exception("Input must no be empty.")
+
+try:
+    i=int(s);
+except ValueError:
+    print "Could not convert data to an integer."
+except:
+    print "Unknown exception!"
+else:
+    print "Your are %d" %i, " years old"
+finally:
+    print "Goodbye!"
+
+hostname = socket.gethostname()
+hostip = socket.gethostbyname(hostname)
+
+print hostname
+print hostip
+
+#get local broadcast address
+list = hostip.split('.')
+print list
+list[-1] = '255'
+print list
+broadcast = ".".join(list)
+
+#broadcast = "255.255.255.255"
+
+#print local information
+print "host name: ", hostname
+print "host ip address: ", hostip
+print "broadcast address: ", broadcast
